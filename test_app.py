692579b2a0d948b07094df6e798cabd199aaf111
marciobacci <marciobacci@gmail.com>
2026-02-10 00:23:20 -0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_healthz_ok():
     client = app.test_client()
     resp = client.get("/healthz")
     assert resp.status_code == 200
-    assert resp.json["status"] == "ok"
+    assert resp.json["status"] ==  "ok"
